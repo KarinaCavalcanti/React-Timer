e2a75939c853d055965d7353115011e728204a97
Karina Lucindo <karina.lucindo@outlook.com>
2022-07-18 15:53:35 -0300

    ESlint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <>Hello, world!</>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
