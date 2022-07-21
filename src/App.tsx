1f9805c3ad77eff9188d23d844091bca42b7233a
Karina Lucindo <karina.lucindo@outlook.com>
2022-07-21 08:56:55 -0300

    Learning about props in styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
-    <>Hello, world!</>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+    </>
   )
 }
